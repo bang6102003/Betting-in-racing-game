b28dfb85150111cb63b17a5cce6adcf676d5dbbe
Tran Bang <tranxuanbang2003@gmail.com>
2023-06-08 13:42:05 +0700

    demo game

---
+++ demoGame/target/classes/main/resources/assets/tilesets/decor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="decor" tilewidth="32" tileheight="32" tilecount="308" columns="22">
+ <image source="../../../../../../../../Downloads/decor-PhotoRoom.png-PhotoRoom.png" width="710" height="473"/>
+</tileset>
